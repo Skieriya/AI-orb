795ff1e61a3ee68b017e928f9670cca8990091ce
Skeiriya Lance <prathamkadam2003@gmail.com>
2025-06-09 12:04:12 +0000

    Noww put a text on the top left corner of hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { InteractiveOrb } from '@/components/interactive-orb';
 export default function AiOrbPage() {
   return (
     <main className="relative w-screen h-screen overflow-hidden bg-background font-body">
+      <div className="absolute top-4 left-4 text-foreground">
+        hello
+      </div>
       <InteractiveOrb />
     </main>
   );
