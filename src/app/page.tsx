8a8d86d262af341b2fa1a93661cddb142ea29e57
Skeiriya Lance <prathamkadam2003@gmail.com>
2025-06-10 08:23:12 +0000

    also remove the hello on top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,7 @@ import { InteractiveOrb } from '@/components/interactive-orb';
 export default function AiOrbPage() {
   return (
     <main className="relative w-screen h-screen overflow-hidden font-body">
-      {/* The next/image component previously here for background has been removed */}
-      <div className="absolute top-4 left-4 text-foreground">
-        hello
-      </div>
+      {/* The div containing "hello" has been removed from here */}
       <InteractiveOrb />
     </main>
   );
